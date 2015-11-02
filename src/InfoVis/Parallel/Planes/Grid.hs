ca1ef8541ecae8fd67669b06c18d1803618c73f3
Brian W Bush <b.w.bush@acm.org>
2015-11-01 18:11:44 -0700

    testing selection

--- src/InfoVis/Parallel/Planes/Grid.hs
+++ src/InfoVis/Parallel/Planes/Grid.hs
@@ -20,31 +20,37 @@ drawSelector =
     drawFaces $ coneFaces height radius
 
 
-drawPlanes :: Int -> Int -> IO ()
-drawPlanes planes divisions =
+drawPlanes :: [(Int, Int, Int)] -> Int -> Int -> IO ()
+drawPlanes selections planes divisions =
   do
     let
       spacing = 2 / fromIntegral (planes - 1) :: GLfloat
       size    = 2 / fromIntegral divisions    :: GLfloat
-      frameGray = 0.4 :: GLfloat
-      faceGray  = 0.2 :: GLfloat
-      frameOpacity = 0.4 :: GLfloat
-      faceOpacity  = 0.2 :: GLfloat
+      frameGray      = 0.4 :: GLfloat
+      faceGray       = 0.2 :: GLfloat
+      selectionColor = 0.6 :: GLfloat
+      frameOpacity     = 0.4 :: GLfloat
+      faceOpacity      = 0.2 :: GLfloat
+      selectionOpacity = 0.8 :: GLfloat
       thickness = size / 20
       faces = brickFaces thickness size size
-      drawBrick =
+      drawBrick selected =
         do
-          color4 (faceGray, faceGray, faceGray, faceOpacity)
+          color4 $
+            if selected
+              then (selectionColor, selectionColor, 0, selectionOpacity)
+              else (faceGray, faceGray, faceGray, faceOpacity)
           drawFaces faces
           color4 (frameGray, frameGray, frameGray, frameOpacity)
           drawFrame faces
     sequence_
       [
         preservingMatrix $ do
-          translate $ vector3 ((fromIntegral i - 1) * spacing - 1, (fromIntegral j - 1) * size - 1, (fromIntegral k - 1) * size)
-          drawBrick
+          translate $ vector3 ((fromIntegral i - 1) * spacing - 1, (fromIntegral j - 1) * size - 1, (fromIntegral k - 1) * size - 1)
+          drawBrick selected
       |
         i <- [1..planes]
       , j <- [1..divisions]
       , k <- [1..divisions]
+      , let selected = (i, j, k) `elem` selections
       ]
--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ module Main (
 ) where
 
 
+import Control.Applicative ((<$>))
 import Data.Default (Default, def)
 import Data.IORef (IORef, newIORef)
 import Graphics.Rendering.Handa.Viewer (ViewerParameters(displayAspectRatio))
@@ -19,8 +20,8 @@ main :: IO ()
 main =
   do
     (dlp, viewerParameters, _) <- setup "Parallel Planes"
-    location <- newIORef (Vector3 0 0 (-2) :: Vector3 GLfloat)
-    tracking <- newIORef $ def {trackPosition = Vector3 0 0 2}
+    location <- newIORef (Vector3 0 0 (-1) :: Vector3 GLfloat)
+    tracking <- newIORef $ def {trackPosition = Vector3 0 0 1.1}
     spaceNavigatorCallback $=! Just (spaceNavigator tracking)
     keyboardMouseCallback $=! Just (keyboardPosition (Vector3 (-0.05) (-0.05) (-0.05)) location)
     displayCallback $=!
@@ -39,6 +40,10 @@ spaceNavigator tracking =
 display :: GLdouble -> IORef (Vector3 GLfloat) -> IORef Track -> DisplayCallback
 display aspect location tracking =
   do
+    Vector3 x y z <- trackPosition <$> get tracking
+    let
+      selection@(ix, _, _) = (round $ (x + 1) / 2 * 4 + 1, round $ (y * realToFrac aspect + 1) / 2 * 10 + 0.5, round $ (z + 1) / 2 * 10 + 0.5)
+      selections = [selection | abs ((x + 1) / 2 * 4 + 1 - fromIntegral ix) < 0.15]
     location' <- get location
     translate location'
     clear [ColorBuffer, DepthBuffer]
@@ -47,4 +52,4 @@ display aspect location tracking =
       drawSelector
     preservingMatrix $ do
       scale 1 (1 / aspect) 1
-      drawPlanes 5 10
+      drawPlanes selections 5 10
