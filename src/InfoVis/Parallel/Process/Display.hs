f8b9488aadd4d46b299d9ab347fec6879172af76
Brian W Bush <b.w.bush@acm.org>
2017-03-11 10:11:25 -0700

    HIVPP-22 aligned text

--- src/InfoVis/Parallel/Process/Display.hs
+++ src/InfoVis/Parallel/Process/Display.hs
@@ -29,10 +29,12 @@ import InfoVis.Parallel.Types.Message (DisplayerMessage(..))
 import InfoVis.Parallel.Types.Presentation (Presentation(..))
 import InfoVis.Parallel.Types.World (World(..))
 import Linear.Affine (Point(..), (.+^))
+import Linear.Conjugate (Conjugate, conjugate)
+import Linear.Epsilon (Epsilon)
 import Linear.Metric (dot, normalize)
 import Linear.Quaternion (Quaternion(..))
 import Linear.V3 (V3(..), cross)
-import Linear.Vector ((*^), zero)
+import Linear.Vector ((^-^), (*^), zero)
 
 import qualified Graphics.Rendering.DLP as D (DlpEncoding(..))
 import qualified Linear.Quaternion as Q (rotate)
@@ -198,15 +200,38 @@ displayer Configuration{..} displayIndex (texts, grids, links) messageVar readyV
                   renderString Roman textContent
               |
                 DisplayText{..} <- texts
+              , let
+                  fromTwoVectors :: (Epsilon a, Floating a) => V3 a -> V3 a -> Quaternion a
+                  fromTwoVectors v1 v2 =
+                    let v1n = normalize v1
+                        v2n = normalize v2
+                        v12n = normalize $ v1n + v2n
+                    in
+                      Quaternion (v12n `dot` v2n) $ v12n `cross` v2n
+                  projectPlane :: (Epsilon a, Floating a) => V3 a -> V3 a -> V3 a
+                  projectPlane v u =
+                    let
+                      un = normalize u
+                    in
+                      v ^-^ (v `dot` un) *^ un
+                  realign :: (Conjugate a, Epsilon a, RealFloat a) => V3 a -> V3 a -> V3 a -> V3 a -> Quaternion a
+                  realign u0 v0 u2 v2 =
+                    let
+                      q2 = fromTwoVectors u0 u2
+                      v1 = conjugate q2 `Q.rotate` v2
+                      v0p = v0 `projectPlane` u0
+                      v1p = v1 `projectPlane` u0
+                      q1 = fromTwoVectors v0p v1p
+                    in
+                      normalize $ q2 * q1
               , let P (V3 xo yo zo) = realToFrac <$> textOrigin
               , let P (V3 xw yw zw) = realToFrac <$> textWidth
               , let P (V3 xh yh zh) = realToFrac <$> textHeight
               , let s = realToFrac textSize * (realToFrac $ baseSize world) / h
-              , let v1 = normalize $ V3 1         0         0
-                    v2 = normalize $ V3 (xw - xo) (yw - yo) (zw - zo)
-                    v = normalize $ v1 + v2
-                    qrot = Quaternion (v `dot` v2) $ v `cross` v2
+              , let qrot = realign (V3 1 0 0) (V3 0 (-1) 0) (V3 (xw - xo) (yw - yo) (zw - zo)) (V3 (xh - xo) (yh - yo) (zh - zo))
 {-
+http://robokitchen.tumblr.com/post/67060392720/finding-a-rotation-quaternion-from-two-pairs-of
+http://stackoverflow.com/questions/4670070/deriving-axis-angle-rotation-from-two-pairs-of-three-points-or-two-pairs-of-tw
 Before rotation: u0, v0. After rotation: u2, v2.
 Quaternion q2 = Quaternion::fromTwoVectors(u0, u2);
 Vector v1 = v2.rotate(q2.conjugate());
