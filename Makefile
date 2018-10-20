9fa6a83d51b2094505c52db0c2a6184e51921a53
Brian W Bush <consult@brianwbush.info>
2018-10-19 22:16:02 -0600

    initial revision

---
+++ Makefile
@@ -0,0 +1,30 @@
+SHELL=bash
+
+
+build: src/Infovis/Protobuf.cs
+	dotnet build
+
+run: src/Infovis/Protobuf.cs
+	bash -c "cd $(<D); dotnet run"
+
+clean:
+	dotnet clean
+	dotnet restore
+
+infovis-unity.sln:
+	dotnet new sln -o ../infovis-unity
+
+src/Infovis/Infovis.csproj:
+	dotnet new console -lang 'C#' -o $(@D)
+	dotnet add $@ package Google.Protobuf --version 3.4.0
+	dotnet sln add $@
+
+src/Infovis/Protobuf.cs: infovis.proto3
+	protoc --csharp_out $(@D) $<
+	mv src/Infovis/Infovis.cs $@
+
+
+.PHONY: build clean run
+
+
+.SUFFIXES:
---
+++ src/Infovis/Program.cs
@@ -0,0 +1,36 @@
+﻿using Google.Protobuf;
+using Infovis.Protobuf;
+using System;
+using System.IO;
+
+
+namespace Infovis
+{
+
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      Response x = new Response {
+        Log = "a message"
+      };
+      Console.WriteLine(x);
+ 
+      byte[] serialized;
+      if (true) {
+        serialized = x.ToByteArray();
+      } else {
+        using (MemoryStream stream = new MemoryStream()) {
+          x.WriteTo(stream);
+          serialized = stream.ToArray();
+        }
+      }
+      Response y = Response.Parser.ParseFrom(serialized);
+
+      Console.WriteLine(y);
+    }
+
+  }
+
+}
