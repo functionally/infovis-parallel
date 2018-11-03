f40034f436ec12411e1bb1b1cdc526d3db3a01cc
Brian W Bush <consult@brianwbush.info>
2018-11-03 10:37:52 -0600

    added monadic logging
    Former-commit-id: f4152e42ed5d174e9400c8b9ee259f27aa62a6c6

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -13,9 +13,13 @@
 -----------------------------------------------------------------------------
 
 
+{-# LANGUAGE ConstraintKinds    #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE FlexibleContexts   #-}
 {-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 
 module Main (
@@ -24,14 +28,18 @@ module Main (
 
 
 import Control.Concurrent
+import Control.Monad (when)
 import Control.Monad.Except (MonadError, MonadIO, liftIO, runExceptT, throwError)
+import Control.Monad.Log (MonadLog, LoggingT, Severity(..), WithSeverity(..), logInfo, renderWithSeverity, runLoggingT)
 import Data.ByteString.Base64 (encode)
 import Data.Data (Data)
+import Data.String (IsString(..))
 import Data.Version (showVersion)
 import Network.WebSockets
 import Paths_infovis_parallel (version)
 import System.Console.CmdArgs (Typeable, (&=), Default(..), args, cmdArgs, details, explicit, help, modes, name, program, summary, typ, typFile)
 import System.Exit (die)
+import System.IO (hPrint, stderr)
 import System.IO.Error (tryIOError)
 
 import qualified Data.ByteString as BS
@@ -42,14 +50,18 @@ stringVersion :: String
 stringVersion = showVersion version ++ ", © 2018"
 
 
+deriving instance Data Severity
+
+
 data Infovis =
     SendBuffers
     {
-      host         :: String
-    , port         :: Int
-    , path         :: String
-    , buffers      :: [FilePath]
+      logging  :: Severity
+    , host     :: String
+    , port     :: Int
+    , path     :: String
     , sendText :: Bool
+    , buffers  :: [FilePath]
     }
     deriving (Data, Show, Typeable)
 
@@ -69,7 +81,12 @@ sendBuffers :: Infovis
 sendBuffers =
   SendBuffers
   {
-    host          = "127.0.0.1"
+    logging       = Notice
+                 &= explicit
+                 &= name "logging"
+                 &= typ "Debug|Informational|Notice|Warning|Error|Critical"
+                 &= help "Level of detail for logging"
+  , host          = "127.0.0.1"
                  &= explicit
                  &= name "host"
                  &= typ "ADDRESS"
@@ -84,13 +101,13 @@ sendBuffers =
                  &= name "path"
                  &= typ "PATH"
                  &= help "Path for the websocket server"
-  , buffers       = def
-                 &= typFile
-                 &= args
   , sendText      = False
                  &= explicit
                  &= name "send-text"
                  &= help "Send data as text messages"
+  , buffers       = def
+                 &= typFile
+                 &= args
   }
     &= name "send-buffers"
     &= help "Send protocol buffers serialized as binary files to client."
@@ -101,33 +118,60 @@ main :: IO ()
 main =
   do
     command <- cmdArgs infovis
-    result <- runExceptT $ dispatch command
+    result <-
+      runExceptT
+        . withSeverityLog (logging command)
+        $ dispatch command
     case result :: Either String () of
       Right () -> return ()
       Left  e  -> die $ "Critical] " ++ e
 
 
-dispatch :: (MonadError String m, MonadIO m)
+dispatch :: (MonadError String m, MonadIO m, SeverityLog m)
          => Infovis
          -> m ()
 
 dispatch SendBuffers{..} =
-  guardIO
-    . runClient host port path
-    $ \connection ->
-    do
-      sequence_
-        [
-          do
-            bytes <- BS.readFile file
-            if sendText
-              then sendBinaryData connection bytes
-              else sendTextData connection $ encode bytes
-        |
-          file <- buffers
-        ]
-      threadDelay 500000
-      sendClose connection $ T.pack "Infovis done."
+  do
+    logInfo $ "Opening WebSocket on <ws://" ++ host ++ ":" ++ show port ++ path ++ "> . . ."
+    guardIO
+      . runClient host port path
+      $ \connection ->
+      do
+        sequence_
+          [
+            do
+              bytes <- BS.readFile file
+              if sendText
+                then sendBinaryData connection bytes
+                else sendTextData connection $ encode bytes
+          |
+            file <- buffers
+          ]
+        threadDelay 500000
+        sendClose connection $ T.pack "Infovis done."
+    logInfo $ "Closing WebSocket on <ws://" ++ host ++ ":" ++ show port ++ path ++ "> . . ."
+
+
+type SeverityLog = MonadLog (WithSeverity String)
+
+
+type SeverityLogT = LoggingT (WithSeverity String)
+
+
+withSeverityLog :: (MonadError String m, MonadIO m)
+                => Severity
+                -> SeverityLogT m a
+                -> m a
+withSeverityLog severity =
+  flip runLoggingT
+    (
+      \message ->
+        when (msgSeverity message <= severity)
+          $ if msgSeverity message > Critical
+              then liftIO . hPrint stderr $ renderWithSeverity fromString message
+              else throwError . fromString $ discardSeverity message
+    )
 
 
 guardIO :: (MonadIO m, MonadError String m) => IO a -> m a
