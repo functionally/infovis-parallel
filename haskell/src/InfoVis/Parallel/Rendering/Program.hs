e8e189c680c5be43fe9edb6793b1ba6192bd4b9f
Brian W Bush <consult@brianwbush.info>
2018-12-09 09:19:25 -0700

    HIVPP-94 module for shader programs
    Former-commit-id: 8fbb5401be14a6cbd3c478e87a0f91aa947d170b

---
+++ haskell/src/InfoVis/Parallel/Rendering/Program.hs
@@ -0,0 +1,173 @@
+{-# LANGUAGE RecordWildCards #-}
+
+
+module InfoVis.Parallel.Rendering.Program (
+  prepareShapeProgram
+, selectProgram
+, setProjectionModelView
+, bindMesh
+, bindPositions
+, bindRotations
+, bindScales
+, bindColors
+) where
+
+
+import Control.Monad (when)
+import Foreign.Ptr (nullPtr)
+import Graphics.GL.ARB.InstancedArrays (glVertexAttribDivisorARB)
+import Graphics.GL.Types (GLfloat, GLuint)
+import Graphics.Rendering.OpenGL.GL (($=!), get)
+import Graphics.Rendering.OpenGL.GL.BufferObjects (BufferObject, BufferTarget(..), bindBuffer)
+import Graphics.Rendering.OpenGL.GL.CoordTrans(GLmatrix, MatrixOrder(RowMajor), newMatrix)
+import Graphics.Rendering.OpenGL.GL.Shaders.Attribs (attribLocation)
+import Graphics.Rendering.OpenGL.GL.Shaders.ProgramObjects (Program, attachShader, createProgram, currentProgram, linkProgram, programSeparable)
+import Graphics.Rendering.OpenGL.GL.Shaders.ShaderObjects (ShaderType(VertexShader), compileShader, createShader, shaderSourceBS)
+import Graphics.Rendering.OpenGL.GL.Shaders.Uniform(UniformLocation, uniform, uniformLocation)
+import Graphics.Rendering.OpenGL.GL.VertexArrays (Capability(..), DataType(Float, UnsignedInt), VertexArrayDescriptor(..), vertexAttribArray, vertexAttribPointer)
+import Graphics.Rendering.OpenGL.GL.VertexSpec (AttribLocation(..), IntegerHandling(KeepIntegral, ToFloat))
+import Linear.Matrix(M44, (!*!))
+import Linear.V4 (V4(..))
+
+import qualified Data.ByteString.Char8 as BS (ByteString, pack)
+
+
+vertexShaderSource :: BS.ByteString
+vertexShaderSource =
+  BS.pack
+    "#version 130                                                                                             \n\
+    \                                                                                                         \n\
+    \uniform mat4 projection_modelview;                                                                       \n\
+    \                                                                                                         \n\
+    \in vec3 mesh_position;                                                                                   \n\
+    \                                                                                                         \n\
+    \in vec3 instance_position;                                                                               \n\
+    \in vec3 instance_scale   ;                                                                               \n\
+    \in vec4 instance_rotation;                                                                               \n\
+    \in uint instance_color   ;                                                                               \n\
+    \                                                                                                         \n\
+    \vec3 rotate(vec3 p, vec4 q) {                                                                            \n\
+    \  return p + 2 * cross(q.xyz, cross(q.xyz, p) + q.w * p);                                                \n\
+    \}                                                                                                        \n\
+    \                                                                                                         \n\
+    \void main() {                                                                                            \n\
+    \  vec4 position = vec4(rotate(mesh_position * instance_scale, instance_rotation) + instance_position, 1);\n\
+    \  vec4 color = vec4(float((0xFF000000u & instance_color) >> 24) / 255,                                   \n\
+    \                    float((0x00FF0000u & instance_color) >> 16) / 255,                                   \n\
+    \                    float((0x0000FF00u & instance_color) >>  8) / 255,                                   \n\
+    \                    float( 0x000000FFu & instance_color       ) / 255);                                  \n\
+    \  gl_Position = projection_modelview * position;                                                         \n\
+    \  gl_FrontColor = color;                                                                                 \n\
+    \}"
+
+
+data ShapeProgram =
+  ShapeProgram
+  {
+    program              :: Program
+  , pmvLocation          :: UniformLocation
+  , meshLocation         :: AttribLocation
+  , positionsLocation    :: AttribLocation
+  , rotationsLocation    :: AttribLocation
+  , scalesLocation       :: AttribLocation
+  , colorsLocation       :: AttribLocation
+  , meshDescription      :: (IntegerHandling, VertexArrayDescriptor GLfloat)
+  , positionsDescription :: (IntegerHandling, VertexArrayDescriptor GLfloat)
+  , rotationsDescription :: (IntegerHandling, VertexArrayDescriptor GLfloat)
+  , scalesDescription    :: (IntegerHandling, VertexArrayDescriptor GLfloat)
+  , colorsDescription    :: (IntegerHandling, VertexArrayDescriptor GLuint )
+  }
+
+
+prepareShapeProgram :: IO ShapeProgram
+prepareShapeProgram =
+  do
+
+    program <- createProgram
+    programSeparable program $=! True
+
+    vertexShader <- createShader VertexShader
+    shaderSourceBS vertexShader $=! vertexShaderSource
+    compileShader vertexShader
+    program `attachShader` vertexShader 
+
+    linkProgram program
+
+    let
+      meshDescription      = (ToFloat     , VertexArrayDescriptor 3 Float       12 nullPtr)
+      positionsDescription = (ToFloat     , VertexArrayDescriptor 3 Float       12 nullPtr)
+      rotationsDescription = (ToFloat     , VertexArrayDescriptor 4 Float       16 nullPtr)
+      scalesDescription    = (ToFloat     , VertexArrayDescriptor 3 Float       12 nullPtr)
+      colorsDescription    = (KeepIntegral, VertexArrayDescriptor 1 UnsignedInt  4 nullPtr)
+
+    pmvLocation       <- get $ uniformLocation program "projection_modelview"
+
+    meshLocation      <- get $ attribLocation  program "mesh_position"
+    positionsLocation <- get $ attribLocation  program "instance_position"
+    rotationsLocation <- get $ attribLocation  program "instance_rotation"
+    scalesLocation    <- get $ attribLocation  program "instance_scale"
+    colorsLocation    <- get $ attribLocation  program "instance_color"
+
+    return ShapeProgram{..}
+
+
+selectProgram :: Maybe ShapeProgram
+              -> IO ()
+selectProgram = (currentProgram $=!) . fmap program
+
+
+setProjectionModelView :: ShapeProgram
+                       -> M44 GLfloat
+                       -> M44 GLfloat
+                       -> IO ()
+setProjectionModelView ShapeProgram{..} projection modelView =
+  do
+    let
+      (V4 (V4 a00 a01 a02 a03) (V4 a10 a11 a12 a13) (V4 a20 a21 a22 a23) (V4 a30 a31 a32 a33)) = projection !*! modelView
+    matrix <- newMatrix RowMajor [a00, a01, a02, a03, a10, a11, a12, a13, a20, a21, a22, a23, a30, a31, a32, a33] :: IO (GLmatrix GLfloat)
+    uniform pmvLocation $=! matrix
+
+
+bindMesh :: ShapeProgram
+         -> Maybe BufferObject
+         -> IO ()
+bindMesh ShapeProgram{..} = bindAttributes False meshLocation meshDescription 
+
+
+bindPositions :: ShapeProgram
+              -> Maybe BufferObject
+              -> IO ()
+bindPositions ShapeProgram{..} = bindAttributes True positionsLocation positionsDescription 
+
+
+bindRotations :: ShapeProgram
+              -> Maybe BufferObject
+              -> IO ()
+bindRotations ShapeProgram{..} = bindAttributes True rotationsLocation rotationsDescription 
+
+
+bindScales :: ShapeProgram
+           -> Maybe BufferObject
+           -> IO ()
+bindScales ShapeProgram{..} = bindAttributes True scalesLocation scalesDescription 
+
+
+bindColors :: ShapeProgram
+           -> Maybe BufferObject
+           -> IO ()
+bindColors ShapeProgram{..} = bindAttributes True colorsLocation colorsDescription 
+
+
+bindAttributes :: Bool
+               -> AttribLocation
+               -> (IntegerHandling, VertexArrayDescriptor a)
+               -> Maybe BufferObject
+               -> IO ()
+bindAttributes instanced location@(AttribLocation location') description (Just buffer) =
+  do
+    bindBuffer ArrayBuffer $=! Just buffer
+    vertexAttribPointer location $=! description
+    when instanced
+     $ glVertexAttribDivisorARB location' 1
+    vertexAttribArray location $=! Enabled
+bindAttributes _ location _ Nothing = vertexAttribArray location $=! Disabled
--- haskell/src/Test.hs
+++ haskell/src/Test.hs
@@ -7,61 +7,21 @@ import Control.Monad (when)
 import Data.Array.Storable (newListArray, withStorableArray)
 import Data.Default (def)
 import Data.IORef (IORef, newIORef)
-import Foreign.Ptr (nullPtr)
 import Foreign.Storable (sizeOf)
-import Graphics.GL.ARB.InstancedArrays (glVertexAttribDivisorARB)
 import Graphics.GL.Types (GLfloat, GLuint)
 import Graphics.Rendering.DLP (DlpEncoding(..))
 import Graphics.Rendering.DLP.Callbacks (DlpDisplay(..), dlpDisplayCallback)
 import Graphics.Rendering.OpenGL.GL (($=!), ($~!), genObjectName, get)
 import Graphics.Rendering.OpenGL.GL.BufferObjects (BufferTarget(..), BufferUsage(DynamicDraw), bindBuffer, bufferData)
-import Graphics.Rendering.OpenGL.GL.CoordTrans(GLmatrix, MatrixOrder(RowMajor), newMatrix)
 import Graphics.Rendering.OpenGL.GL.DebugOutput (debugMessageCallback, debugOutput)
 import Graphics.Rendering.OpenGL.GL.PerFragment(ComparisonFunction(Less))
-import Graphics.Rendering.OpenGL.GL.Shaders.Attribs (attribLocation)
-import Graphics.Rendering.OpenGL.GL.Shaders.ProgramObjects (attachShader, createProgram, currentProgram, linkProgram, programSeparable)
-import Graphics.Rendering.OpenGL.GL.Shaders.ShaderObjects (ShaderType(VertexShader), compileShader, createShader, shaderSourceBS)
-import Graphics.Rendering.OpenGL.GL.Shaders.Uniform(uniform, uniformLocation)
 import Graphics.Rendering.OpenGL.GL.Tensor (Vector3(..), Vector4(..))
-import Graphics.Rendering.OpenGL.GL.VertexArrays (Capability(..), DataType(Float, UnsignedInt), VertexArrayDescriptor(..), drawArraysInstanced, vertexAttribArray, vertexAttribPointer)
-import Graphics.Rendering.OpenGL.GL.VertexSpec (AttribLocation(..), IntegerHandling(KeepIntegral, ToFloat))
+import Graphics.Rendering.OpenGL.GL.VertexArrays (Capability(..), drawArraysInstanced)
 import Graphics.UI.GLUT (DisplayMode(..), IdleCallback, createWindow, depthFunc, fullScreen, getArgsAndInitialize, idleCallback, initialDisplayMode, mainLoop, postRedisplay)
 import InfoVis.Parallel.Rendering.NewShapes
-import Linear.Matrix((!*!))
+import InfoVis.Parallel.Rendering.Program
 import Linear.Projection (lookAt, perspective)
 import Linear.V3 (V3(..))
-import Linear.V4 (V4(..))
-
-import qualified Data.ByteString.Char8 as BS (ByteString, pack)
-
-
-vertexShaderSource :: BS.ByteString
-vertexShaderSource =
-  BS.pack
-    "#version 130                                                                                             \n\
-    \                                                                                                         \n\
-    \uniform mat4 projection_modelview;                                                                       \n\
-    \                                                                                                         \n\
-    \in vec3 mesh_position;                                                                                   \n\
-    \                                                                                                         \n\
-    \in vec3 instance_position;                                                                               \n\
-    \in vec3 instance_scale   ;                                                                               \n\
-    \in vec4 instance_rotation;                                                                               \n\
-    \in uint instance_color   ;                                                                               \n\
-    \                                                                                                         \n\
-    \vec3 rotate(vec3 p, vec4 q) {                                                                            \n\
-    \  return p + 2 * cross(q.xyz, cross(q.xyz, p) + q.w * p);                                                \n\
-    \}                                                                                                        \n\
-    \                                                                                                         \n\
-    \void main() {                                                                                            \n\
-    \  vec4 position = vec4(rotate(mesh_position * instance_scale, instance_rotation) + instance_position, 1);\n\
-    \  vec4 color = vec4(float((0xFF000000u & instance_color) >> 24) / 255,                                   \n\
-    \                    float((0x00FF0000u & instance_color) >> 16) / 255,                                   \n\
-    \                    float((0x0000FF00u & instance_color) >>  8) / 255,                                   \n\
-    \                    float( 0x000000FFu & instance_color       ) / 255);                                  \n\
-    \  gl_Position = projection_modelview * position;                                                         \n\
-    \  gl_FrontColor = color;                                                                                 \n\
-    \}"
 
 
 examplePositions :: [Vector3 GLfloat]
@@ -131,20 +91,10 @@ testSetup angle =
     debugOutput $=! Enabled
     debugMessageCallback $=! Just print
 
-    program <- createProgram
-    programSeparable program $=! True
-
-    vertexShader <- createShader VertexShader
-    shaderSourceBS vertexShader $=! vertexShaderSource
-    compileShader vertexShader
-    program `attachShader` vertexShader 
-
-    linkProgram program
-
-    projectionModelViewLoc <- get $ uniformLocation program "projection_modelview"
+    shapeProgram <- prepareShapeProgram
 
     let
-      buildBuffer p k t g i xs =
+      buildBuffer xs =
         do
           let
             m = sizeOf $ head xs
@@ -155,40 +105,28 @@ testSetup angle =
           values <- newListArray (0, size) xs
           withStorableArray values $ \ptr -> bufferData ArrayBuffer $=! (toEnum size, ptr, DynamicDraw)
           bindBuffer ArrayBuffer $=! Nothing
-          l@(AttribLocation l') <- get $ attribLocation program p
-          return
-            (
-              do
-                bindBuffer ArrayBuffer $=! Just bufferObject
-                vertexAttribPointer l $=! (g, VertexArrayDescriptor k t (fromIntegral m) nullPtr)
-                when i
-                 $ glVertexAttribDivisorARB l' 1
-                vertexAttribArray l $=! Enabled
-            , bufferObject
-            )
+          return bufferObject
       (primitiveMode, primitives) = cube (1 :: GLfloat)
 
-    (quadsBinder    , _quadsBO    ) <- buildBuffer "mesh_position"     3 Float       ToFloat      False primitives
-    (positionsBinder, _positionsBO) <- buildBuffer "instance_position" 3 Float       ToFloat      True  examplePositions
-    (rotationsBinder, _rotationsBO) <- buildBuffer "instance_rotation" 4 Float       ToFloat      True  exampleRotations
-    (scalesBinder   , _scalesBO   ) <- buildBuffer "instance_scale"    3 Float       ToFloat      True  exampleScales
-    (colorsBinder   , _colorsBO   ) <- buildBuffer "instance_color"    1 UnsignedInt KeepIntegral True  exampleColors
+    quadsBO     <- buildBuffer primitives
+    positionsBO <- buildBuffer examplePositions
+    rotationsBO <- buildBuffer exampleRotations
+    scalesBO    <- buildBuffer exampleScales
+    colorsBO    <- buildBuffer exampleColors
 
     return
       $ do
-        currentProgram $=! Just program
+        selectProgram $ Just shapeProgram
         angle' <- get angle
         let
           projection = perspective (pi / 3) 1 0.1 10
           modelView = lookAt (V3 (-5 :: GLfloat) (1 +sin angle') (2 + cos angle')) (V3 0 0 0) (V3 0 1 0)
-          (V4 (V4 a00 a01 a02 a03) (V4 a10 a11 a12 a13) (V4 a20 a21 a22 a23) (V4 a30 a31 a32 a33)) = projection !*! modelView
-        projectionModelView <- newMatrix RowMajor [a00, a01, a02, a03, a10, a11, a12, a13, a20, a21, a22, a23, a30, a31, a32, a33] :: IO (GLmatrix GLfloat)
-        uniform projectionModelViewLoc $=! projectionModelView
-        quadsBinder
-        positionsBinder
-        rotationsBinder
-        scalesBinder
-        colorsBinder
+        setProjectionModelView shapeProgram projection modelView 
+        shapeProgram `bindMesh`      Just quadsBO
+        shapeProgram `bindPositions` Just positionsBO
+        shapeProgram `bindRotations` Just rotationsBO
+        shapeProgram `bindScales`    Just scalesBO
+        shapeProgram `bindColors`    Just colorsBO
         drawArraysInstanced
           primitiveMode
           0
