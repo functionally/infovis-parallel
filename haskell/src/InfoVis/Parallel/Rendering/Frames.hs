064d5c7334f71dcae99be0321bcbb54a866368b8
Brian W Bush <consult@brianwbush.info>
2018-12-27 15:52:55 -0700

    HIVPP-94 drawing polylines, rectangles, and arrows
    Former-commit-id: e3eba23faa09ff7cd9b5231c81ba9b417f167bf1

--- haskell/src/InfoVis/Parallel/Rendering/Frames.hs
+++ haskell/src/InfoVis/Parallel/Rendering/Frames.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RecordWildCards  #-}
+{-# LANGUAGE TypeFamilies     #-}
 
 
 module InfoVis.Parallel.Rendering.Frames (
@@ -12,14 +14,19 @@ module InfoVis.Parallel.Rendering.Frames (
 
 
 import Control.Monad (liftM2)
+import Data.List (zipWith4)
+import Debug.Trace (trace)
 import Graphics.GL.Types (GLfloat)
 import Graphics.Rendering.OpenGL.GL.Tensor (Vector3(..), Vector4(..), Vertex3(..))
 import InfoVis.Parallel.NewTypes (Geometry(..), Glyph(..), Identifier, Shape(..))
-import InfoVis.Parallel.Rendering.Buffers (ShapeBuffer, createShapeBuffer, destroyShapeBuffer, drawInstances, insertPositions, updateColor, updateRotations, updateScale, prepareShapeBuffer)
+import InfoVis.Parallel.Rendering.Buffers (ShapeBuffer, createShapeBuffer, destroyShapeBuffer, drawInstances, insertPositions, updateColor, updateRotations, updateScales, prepareShapeBuffer)
 import InfoVis.Parallel.Rendering.NewShapes (Mesh, arrow, cube, icosahedron, square, tube)
 import InfoVis.Parallel.Rendering.Program (ShapeProgram)
-import Linear.Affine (Point(..))
+import Linear.Affine (Point(..), (.-.), (.+^))
 import Linear.Matrix (M44)
+import Linear.Metric (norm)
+import Linear.Quaternion (Quaternion(..), rotate)
+import Linear.Util (rotationFromPlane, rotationFromVectorPair)
 import Linear.V3 (V3(..))
 
 import qualified Data.Map.Strict as M (Map, adjust, elems, empty, fromList, insert, toList)
@@ -42,7 +49,7 @@ mesh SphereMesh    = icosahedron 1
 mesh PolylineMesh  = tube 1 1
 mesh RectangleMesh = square 1
 mesh LabelMesh     = undefined
-mesh AxisMesh      = arrow 1 1 1 1 -- FIXME
+mesh AxisMesh      = arrow 1 1 0.1 2
 
 
 findShapeMesh :: Geometry
@@ -178,25 +185,61 @@ insertGeometryFrame _ geometryFrame@LabelFrame{} =
   geometryFrame
 insertGeometryFrame (identifier, geometry@Geometry{..}) geometryFrame@ShapeFrame{..} =
   let
-    (positions, rotations, scale) =
-      case shape of
-        Points _   pss -> (
-                            (\(P (V3 x y z)) -> realToFrac <$> Vertex3 x y z) <$> concat pss
-                          , replicate (sum $ length <$> pss) $ Vector4 0 0 0 1
-                          , realToFrac <$> Vector3 size size size
-                          )
-        Polylines  pps -> let
+    noRotation = Quaternion 1 $ V3 0 0 0
+    right = V3 1 0 0
+    back = V3 0 0 1
+    toPosition (P (V3 x y z)) = realToFrac <$> Vertex3 x y z
+    toRotation (Quaternion w (V3 x y z)) = realToFrac <$> Vector4 x y z w
+    toScale (V3 x y z) = realToFrac <$> Vector3 x y z
+    (positions, rotations, scales) =
+      unzip3 $ case shape of
+        Points _   pss -> let
+                            make p = (p, noRotation, V3 size size size)
                           in
-                            undefined
-        Rectangles pps -> undefined
+                            make <$> concat pss
+        Polylines  pps -> let
+                            make u0 u1 =
+                              let
+                                ud = u1 .-. u0
+                                uc = u0 .+^ ud / 2
+                              in
+                                (
+                                  uc
+                                , rotationFromVectorPair right ud
+                                , V3 (norm ud) size size
+                                )
+                          in
+                            zipWith make (concatMap init pps) (concatMap tail pps)
+        Rectangles pps -> let
+                            make (o, u, v) =
+                              let
+                                w = norm $ u .-. o
+                                h = norm $ v .-. o
+                                q = rotationFromPlane right back o u v
+                              in
+                                (
+                                  o .+^ q `rotate` V3 (w / 2) 0 (h / 2)
+                                , q
+                                , V3 w size h
+                                )
+                          in
+                            make <$> pps
         Label      _   -> undefined
-        Axis       pps -> undefined
+        Axis  (u0, u1) -> let
+                            ud = u1 .-. u0
+                            uc = u0 .+^ ud / 2
+                          in
+                            [(
+                              uc
+                            , rotationFromVectorPair right ud
+                            , V3 (norm ud) size size
+                            )]
   in
     geometryFrame
     {
       geometries = M.insert identifier geometry geometries
-    , buffer     =   updateColor     (identifier, color    )
-                   . updateScale     (identifier, scale    )
-                   . updateRotations (identifier, rotations)
-                   $ insertPositions (identifier, positions) buffer
+    , buffer     =   updateColor     (identifier,                color    )
+                   . updateScales    (identifier, toScale    <$> scales   )
+                   . updateRotations (identifier, toRotation <$> rotations)
+                   $ insertPositions (identifier, toPosition <$> positions) buffer
     }
