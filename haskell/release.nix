ce5bcf5e6ae8504d11b2cbab87d21adfe6e9c450
Brian W Bush <consult@brianwbush.info>
2019-02-21 07:07:30 -0700

    added kafka package
    Former-commit-id: 7031bdf6193ced2efb0256f7cc0876eb1e39db5e

--- haskell/release.nix
+++ haskell/release.nix
@@ -10,25 +10,8 @@ let
         packages = pkgs.haskell.packages // {
           "${compiler}" = pkgs.haskell.packages."${compiler}".override {
             overrides = haskellPackagesNew: haskellPackagesOld: rec {
-              #mkDerivation = args: haskellPackagesOld.mkDerivation ( args // {
-              ## enableLibraryProfiling = true;
-              ## doCheck = false;
-              ## doHaddock = false;
-              #});
-              infovis-parallel = haskellPackagesNew.callPackage ./default.nix       {                };
-              vulkan-api       = haskellPackagesNew.callPackage ./vulkan-api.nix    { vulkan = null; };
-              GLFW-b           = haskellPackagesNew.callPackage ./GLFW-b.nix        {                };
-              bindings-GLFW    = haskellPackagesNew.callPackage ./bindings-GLFW.nix {
-                                                                                        inherit (pkgs)      libGL;
-                                                                                        inherit (pkgs.xorg) libX11; 
-                                                                                        inherit (pkgs.xorg) libXcursor;
-                                                                                        inherit (pkgs.xorg) libXext; 
-                                                                                        inherit (pkgs.xorg) libXfixes;
-                                                                                        inherit (pkgs.xorg) libXi; 
-                                                                                        inherit (pkgs.xorg) libXinerama;
-                                                                                        inherit (pkgs.xorg) libXrandr; 
-                                                                                        inherit (pkgs.xorg) libXxf86vm;
-                                                                                      };
+              infovis-parallel = haskellPackagesNew.callPackage                  ./default.nix { };
+              kafka-device     = haskellPackagesNew.callPackage ../../kafka-device/default.nix { };
             };
           };
         };
