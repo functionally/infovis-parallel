9689cbc340826e962f3844334e1c2a1f4d9b142a
Brian W Bush <consult@brianwbush.info>
2019-03-02 20:15:58 -0700

    revised test script
    Former-commit-id: c347557a9eb4a977e4d05f0c88609accd8e67801

--- haskell/release.nix
+++ haskell/release.nix
@@ -10,9 +10,11 @@ let
         packages = pkgs.haskell.packages // {
           "${compiler}" = pkgs.haskell.packages."${compiler}".override {
             overrides = haskellPackagesNew: haskellPackagesOld: rec {
-              infovis-parallel  = haskellPackagesNew.callPackage                       ./default.nix { };
-              kafka-device      = haskellPackagesNew.callPackage      ../../kafka-device/default.nix { };
-              kafka-device-glut = haskellPackagesNew.callPackage ../../kafka-device-glut/default.nix { };
+              infovis-parallel      = haskellPackagesNew.callPackage                           ./default.nix { };
+              kafka-device          = haskellPackagesNew.callPackage          ../../kafka-device/default.nix { };
+              kafka-device-glut     = haskellPackagesNew.callPackage     ../../kafka-device-glut/default.nix { };
+              kafka-device-joystick = haskellPackagesNew.callPackage ../../kafka-device-joystick/default.nix { };
+              kafka-device-spacenav = haskellPackagesNew.callPackage ../../kafka-device-spacenav/default.nix { };
             };
           };
         };
@@ -22,5 +24,8 @@ let
   pkgs = nixpkgs { inherit config; };
 in
   {
-    infovis-parallel = pkgs.haskell.packages."${compiler}".infovis-parallel;
+    infovis-parallel      = pkgs.haskell.packages."${compiler}".infovis-parallel     ;
+    kafka-device          = pkgs.haskell.packages."${compiler}".kafka-device         ;
+    kafka-device-joystick = pkgs.haskell.packages."${compiler}".kafka-device-joystick;
+    kafka-device-spacenav = pkgs.haskell.packages."${compiler}".kafka-device-spacenav;
   }
