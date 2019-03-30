bca762a5a9a6503407dbcf5bfaace82dbe5bf17a
Brian W Bush <consult@brianwbush.info>
2019-03-29 18:11:05 -0600

    HIVPP-95 scaling of extents
    Former-commit-id: 7f1932599b09c936b11546d84f427b7b6a49cd70

--- haskell/src/InfoVis/Parallel/Presenter/Extent.hs
+++ haskell/src/InfoVis/Parallel/Presenter/Extent.hs
@@ -1,17 +1,27 @@
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 
 module InfoVis.Parallel.Presenter.Extent (
-  Extent(..)
+  Scalable(..)
+, Extent(..)
 ) where
 
 
 import Data.Aeson.Types (FromJSON(..), ToJSON(..))
 import Data.Binary (Binary)
 import GHC.Generics (Generic)
-import InfoVis.Parallel.Types (Position)
-import Linear.Util.Instances ()
+import InfoVis.Parallel.Types (Geometry(..), Position, Shape(..))
+import Linear.Affine ((.-.), (.+^))
+import Linear.Matrix ((!*), transpose)
+import Linear.V3 (V3(..))
+import Linear.Vector (zero)
+
+
+class Scalable a where
+  scale :: Extent -> a -> a
 
 
 data Extent =
@@ -35,3 +45,41 @@ data Extent =
     }
     deriving (Binary, Eq, FromJSON, Generic, Ord, Read, Show, ToJSON)
 
+
+instance Scalable Position where
+  scale Extent1D{..} = scaling origin cornerX zero    zero
+  scale Extent2D{..} = scaling origin cornerX cornerY zero
+  scale Extent3D{..} = scaling origin cornerX cornerY cornerZ
+
+
+instance Scalable Geometry where
+  scale extent geometry@Geometry{..} = geometry {shape = scale extent shape}
+
+
+instance Scalable Shape where
+  scale extent (Points glyph positions) = Points glyph $ fmap (scale extent) <$> positions
+  scale extent (Polylines    positions) = Polylines    $ fmap (scale extent) <$> positions
+  scale extent (Rectangles   positions) = Rectangles   $ scaleTriplet extent <$> positions
+  scale extent (Label        positions) = Label        $ scaleTriplet extent     positions
+  scale extent (Axis         positions) = Axis         $ scaleDoublet extent     positions
+
+
+scaleDoublet :: Extent
+             -> (Position, Position)
+             -> (Position, Position)
+scaleDoublet extent (p1, p2) = (scale extent p1, scale extent p2)
+
+
+scaleTriplet :: Extent
+             -> (Position, Position, Position)
+             -> (Position, Position, Position)
+scaleTriplet extent (p1, p2, p3) = (scale extent p1, scale extent p2, scale extent p3)
+
+
+scaling :: Position
+        -> Position
+        -> Position
+        -> Position
+        -> Position
+        -> Position
+scaling o u v w p = o .+^ transpose (V3 (u .-. o) (v .-. o) (w .-. o)) !* (p .-. o)
