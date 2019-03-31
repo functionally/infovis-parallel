f3f4b990bb6fc30415c0177c75a61bd2518ef13f
Brian W Bush <consult@brianwbush.info>
2019-03-31 12:52:45 -0600

    HIVPP-95 linkages
    Former-commit-id: de8fa4cdae76f79b967dd02251d3aedc6ad9949d

--- haskell/src/InfoVis/Parallel/Presenter/Link.hs
+++ haskell/src/InfoVis/Parallel/Presenter/Link.hs
@@ -1,17 +1,26 @@
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE DeriveAnyClass  #-}
+{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE RecordWildCards #-}
 
 
 module InfoVis.Parallel.Presenter.Link (
-  LinkAlias
+  LinkDisplay
+, LinkAlias
 , Link(..)
+, linker
+, link
 ) where
 
 
 import Data.Aeson.Types (FromJSON(..), ToJSON(..))
 import Data.Binary (Binary)
+import Data.Maybe (fromJust)
 import GHC.Generics (Generic)
-import InfoVis.Parallel.Presenter.Grid (GridAlias, Styling)
+import InfoVis.Parallel.Presenter.Grid (GridAlias, Styling(..))
+import InfoVis.Parallel.Types (Geometry(..), Glyph(..), Position, Shape(..))
+
+
+type LinkDisplay = [(LinkAlias, Geometry)]
 
 
 type LinkAlias = String
@@ -31,3 +40,36 @@ data Link =
     , styling     :: Styling
     }
     deriving (Binary, Eq, FromJSON, Generic, Ord, Read, Show, ToJSON)
+
+
+linker :: [(GridAlias, Position)]
+       -> Link
+       -> (LinkAlias, Geometry)
+linker positions Point{..} =
+  (
+    linkAlias
+  , Geometry
+    {
+      shape = Points Sphere [[fromJust $ linkedGrid `lookup` positions]]
+    , size  = 0.01
+    , color = normalColor styling
+    , text  = ""
+    }
+  )
+linker positions Polyline{..} =
+  (
+    linkAlias
+  , Geometry
+    {
+      shape = Polylines [[fromJust $ linkedGrid `lookup` positions | linkedGrid <- linkedGrids]]
+    , size  = 0.01
+    , color = normalColor styling
+    , text  = ""
+    }
+  )
+
+
+link :: [Link]
+     -> [(GridAlias, Position)]
+     -> [(LinkAlias, Geometry)]
+link links positions = linker positions <$> links
