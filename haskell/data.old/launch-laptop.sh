6ad4811cd829408ec934b0c651ab530e71543cf8
Brian W Bush <consult@brianwbush.info>
2019-03-31 17:45:29 -0600

    removed old example files
    Former-commit-id: 504511ef7f1c77543521c4d8866dae558ba27884

--- haskell/data.old/launch-laptop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CFG="$* laptop/world.yaml laptop/input.yaml laptop/advanced.yaml"
-EXE=../dist/build/infovis-parallel/infovis-parallel
-
-echo Configutation: $CFG
-
-$EXE +RTS -N6 -RTS solo $CFG
--- haskell/data.old/launch-zspace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CFG="$* zspace/world.yaml zspace/input-vrpn.yaml zspace/advanced.yaml"
-EXE=../dist/build/infovis-parallel/infovis-parallel
-
-echo Configutation: $CFG
-
-$EXE +RTS -N6 -RTS solo $CFG
