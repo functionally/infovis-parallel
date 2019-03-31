4321bc13d96080241a307ac63fb57dce1df42f94
Brian W Bush <consult@brianwbush.info>
2019-03-30 18:42:21 -0600

    make file for launching services
    Former-commit-id: c328f1ca9ecdce5c4317be256e82c1792df17d0a

---
+++ haskell/data/shell.nix
@@ -0,0 +1,40 @@
+{
+  nixpkgs  ? null
+, compiler ? "ghc822"
+}:
+
+let
+
+  nixpkgs1 =
+    if nixpkgs == null
+      then let
+             bootstrap = import <nixpkgs> { };
+             location = builtins.fromJSON (builtins.readFile ../nixpkgs.json);
+             src = bootstrap.fetchFromGitHub {
+               owner = "NixOS";
+               repo  = "nixpkgs";
+               inherit (location) rev sha256;
+             };
+           in
+            import src
+      else nixpkgs;
+
+  pkgs = nixpkgs1 { };
+
+  release = (import ../release.nix) {nixpkgs = nixpkgs1; compiler = compiler;};
+
+in
+
+  pkgs.stdenv.mkDerivation {
+    name = "env-ivp";
+    buildInputs = [
+      pkgs.gnumake
+      pkgs.jre
+      pkgs.zookeeper
+      pkgs.apacheKafka011
+      pkgs.kafkacat
+#     release.infovis-parallel
+      release.kafka-device-joystick
+      release.kafka-device-spacenav
+    ];
+  }
