9ce5c7e40d88b3368ba72677b8bb042fa57f43b3
Brian W Bush <consult@brianwbush.info>
2019-03-31 17:45:29 -0600

    removed old example files
    Former-commit-id: 27d2bd1b4a015edf8d5cf409379cbf5cbf5dd0bf

--- haskell/data.old/demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kafka-device-spacenav laptop/spacenav.yaml &
-kafka-device-spacenav laptop/joystick.yaml &
-
-../dist/build/infovis-parallel/infovis-parallel +RTS -N6 -RTS solo ENB2012.yaml presentation.yaml demo/world.yaml demo/input.yaml &
-
-wait
