52905c2f785ac3e5bc24ccd0725274b556cc9fb2
Brian W Bush <brian.bush@nrel.gov>
2019-10-17 16:49:04 -0600

    basic support for websockets
    Former-commit-id: f80e65e467eff9c8fea06d64e01da63896840cfb

--- web/src/controller.js
+++ web/src/controller.js
@@ -16,10 +16,34 @@ function startup() {
 }
 
 
-var msg = proto.Infovis.Geometry()
+function connect(url, handler) {
+  var connection = new WebSocket(url)
+  connection.binaryType = "arraybuffer"
+  handlers = new Object()
+  connection.onmessage =
+    function(event) {
+      var buffer = new Uint8Array(event.data);
+      var request = proto.Infovis.Request.deserializeBinary(buffer)
+      handler(connection, request)
+    }
+  return connection
+}
+
+
+function disconnect(connection) {
+  connection.close(1000, "normal termination");
+}
+
+
+function echoHandler(connection, request) {
+  console.log("Request: ", request)
+}
 
 
 // Export functions.
 module.exports = {
-  startup: startup
+  startup     : startup
+, connect     : connect
+, disconnect  : disconnect
+, echoHandler : echoHandler
 }
