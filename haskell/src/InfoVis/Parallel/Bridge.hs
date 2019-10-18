026580dbc525ae61f44e9eb3bdfa126acaed0a21
Brian W Bush <brian.bush@nrel.gov>
2019-10-17 18:33:30 -0600

    added mode to serve buffers
    Former-commit-id: 01e319738591970891b57e97b5e35a9549a49fae

---
+++ haskell/src/InfoVis/Parallel/Bridge.hs
@@ -0,0 +1,55 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  $Headers
+-- Copyright   :  (c) 2018 National Renewable Energy Laboratory
+-- License     :  All Rights Reserved
+--
+-- Maintainer  :  Brian W Bush <brian.bush@nrel.gov>
+-- Stability   :  Stable
+-- Portability :  Portable
+--
+-- | Sending buffers to Infovis-Parallel servers over WebSockets.
+--
+-----------------------------------------------------------------------------
+
+
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+
+module InfoVis.Parallel.Bridge (
+  bridgeKafka
+) where
+
+
+import Control.Monad.Except (MonadError, MonadIO)
+import Control.Monad.Log (Severity(..), logInfo)
+import InfoVis (SeverityLog, withLogger)
+import Network.WebSockets (acceptRequest, runServer, sendBinaryData)
+
+import qualified Data.ByteString as BS (readFile)
+
+
+bridgeKafka :: (MonadError String m, MonadIO m, SeverityLog m)
+            => String
+            -> Int
+            -> [FilePath]
+            -> m ()
+bridgeKafka host port buffers =
+  do
+    logInfo $ "Serving WebSockets on <ws://" ++ host ++ ":" ++ show port ++ "> . . ."
+    withLogger $ \logger ->
+      runServer host port
+        $ \pending ->
+        do
+          connection <- acceptRequest pending
+          sequence_
+            [
+              do
+                logger Debug $ "Reading " ++ show file ++ " . . ."           
+                bytes <- BS.readFile file
+                logger Informational $ "Sending " ++ show file ++ " . . ."           
+                sendBinaryData connection bytes
+            |
+              file <- buffers
+            ]
