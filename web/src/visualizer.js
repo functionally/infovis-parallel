0564f6cb0be1699015608423427bcdc1b1cdf084
Brian W Bush <brian.bush@nrel.gov>
2019-10-25 13:27:06 -0600

    cleaned up clearing of canvas
    Former-commit-id: 7349dac0ef51b522f559f88c86a7d9cd4cf93b73

--- web/src/visualizer.js
+++ web/src/visualizer.js
@@ -1,10 +1,10 @@
 
 const Rendering = {
-  Frames     : require("./rendering/frames"  )
-, Linear     : require("./rendering/linear"  )
+  Frames     : require("./rendering/frames"    )
+, Linear     : require("./rendering/linear"    )
 , Program    : require("./rendering/program"   )
 , Projection : require("./rendering/projection")
-, Selector   : require("./rendering/selector")
+, Selector   : require("./rendering/selector"  )
 }
 
 require("./gl-matrix")
@@ -18,21 +18,27 @@ const zero = vec3.fromValues(0, 0, 0)
 
 function setupCanvas(gl) {
 
-  gl.enable(gl.DEPTH_TEST)
-  gl.depthFunc(gl.LESS)
-
-  gl.enable(gl.BLEND)
-  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
-
-  // FIXME: Experiment to see if this works well with alpha.
-  if (false)
-    gl.enable(gl.CULL_FACE)
+  console.debug("setupCanvas")
 
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
   gl.clearColor(0., 0., 0., 1.)
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+  gl.clearDepth(1.0)
 
+  gl.enable(gl.DEPTH_TEST)
+  gl.depthFunc(gl.LEQUAL)
+
+  // FIXME: Test before using.
+  if (false) {
+    gl.enable(gl.BLEND)
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+  }
+
+  // FIXME: Test before using.
+  if (false)
+    gl.enable(gl.CULL_FACE)
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 }
 
 
@@ -65,6 +71,7 @@ function visualizeBuffers(gl, configuration, requests) {
 
   function animation(timestamp) {
 
+
     if (requests.length > 0) {
 
       while (requests.length > 0) {
@@ -73,24 +80,34 @@ function visualizeBuffers(gl, configuration, requests) {
         Rendering.Frames.insert(gl, request.getUpsertList(), graphics.manager)
       }
 
+      setupCanvas(gl)
+
       Rendering.Program.selectShapeProgram(gl, graphics.manager.program)
 
       Rendering.Frames.prepare(gl, graphics.manager)
       Rendering.Selector.prepare(gl, graphics.tool.position, graphics.tool.rotation, graphics.selector)
 
-let projection = Rendering.Projection.projection(configuration.display, graphics.pov.position)
-console.debug("animation: projection1 =", projection)
-      const modelView = Rendering.Projection.modelView(graphics.offset.position, graphics.offset.rotation)
-      projection = mat4.fromValues(
-        6.909721374511719,                0.0, -1.7361111640930176, -3.4548587799072266
-      , 0.0              , 12.134145736694336, -1.8292683362960815, -6.067071914672852
-      , 0.0              ,  0.0              , -1.0002000331878662,  9.931987762451172
-      , 0.0              ,  0.0              , -1.0               ,  9.949999809265137
-      )
-      const pmv = Rendering.Program.setProjectionModelView(gl, graphics.manager.program, projection, modelView)
+      // FIXME: Test before using.
+      if (false) {
 
-      gl.clearColor(0., 0., 0., 1.)
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+        const  projection = Rendering.Projection.projection(configuration.display, graphics.pov.position)
+        const modelView = Rendering.Projection.modelView(graphics.offset.position, graphics.offset.rotation)
+        Rendering.Program.setProjectionModelView(gl, graphics.manager.program, projection, modelView)
+
+      } else {
+
+        console.debug("animation: <alternative pmv>")
+        const fieldOfView = 45 * Math.PI / 180;   // in radians
+        const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+        const zNear = 0.1;
+        const zFar = 100.0;
+        const projection = mat4.create();
+        mat4.perspective(projection, fieldOfView, aspect, zNear, zFar);
+        const modelView = mat4.create();
+        mat4.translate(modelView, modelView, [-0.0, 0.0, -15.0]);
+        Rendering.Program.setProjectionModelView(gl, graphics.manager.program, projection, modelView)
+
+      }
 
       Rendering.Frames.draw(gl, graphics.manager)
       Rendering.Selector.draw(gl, graphics.selector)
