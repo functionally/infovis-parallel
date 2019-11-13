48a2e71a4aa694e73f9c6abe426d1ce0a4e91b82
Brian W Bush <brian.bush@nrel.gov>
2019-11-12 21:08:35 -0700

    script for running demo
    Former-commit-id: 90ac10c2cd6355972715817f91ef1d5efb1e12f6

---
+++ go/demo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -x
+
+./go-infovis --demo 0.0.0.0:8081  /infovis/v4/demo  \
+             ../protobuf/examples/axes.pbb          \
+             ../protobuf/examples/corner-points.pbb \
+             ../protobuf/examples/rectangles.pbb    \
+             ../protobuf/examples/helices.pbb       \
+             ../protobuf/examples/bslm-??.pbb
