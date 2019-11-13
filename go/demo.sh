2d009e074f596f9674c72a1c370c731f7d57c56f
Brian W Bush <brian.bush@nrel.gov>
2019-11-13 07:20:49 -0700

    organized deployment parameters
    Former-commit-id: 6b1f3c9bd19ff11f5e94ee6577e6b82bdbd1e3b3

--- go/demo.sh
+++ go/demo.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-./go-infovis --demo 0.0.0.0:8081  /infovis/v4/demo  \
+./go-infovis --demo 0.0.0.0:42042 /infovis/v4/demo  \
              ../protobuf/examples/axes.pbb          \
              ../protobuf/examples/corner-points.pbb \
              ../protobuf/examples/rectangles.pbb    \
--- web/src/configuration.js
+++ web/src/configuration.js
@@ -13,7 +13,7 @@ function defaultConfiguration(w = window.innerWidth, h = window.innerHeight) {
 
   return {
     server : {
-      address        : "ws://104.198.152.159:80/infovis/v4/demo"
+      address        : "ws://104.198.152.159:42042/infovis/v4/demo"
     }
   , display          : {
       stereo         : false
