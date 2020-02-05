4bf7a1a4370619d1d5b400cda23854c9fc4840d6
Brian W Bush <brian.bush@nrel.gov>
2020-02-05 14:36:28 -0700

    updated documentation and added example devices
    Former-commit-id: 8b5ab9adc8bef73b144cc41b6a11f70308d84f19

---
+++ docs/slides.sh
@@ -0,0 +1,1 @@
+pandoc --self-contained -f markdown+smart --to slidy --slide-level=2 --output infovis-parallel.html infovis-parallel.md
--- slides.sh
+++
@@ -1,1 +0,0 @@
-pandoc --self-contained -f markdown+smart --to slidy --slide-level=2 --output ivp-slides.html infovis-parallel.md
