0a7775272e8da42e612d7dc2e25a2cba626dba45
Brian W Bush <brian.bush@nrel.gov>
2020-11-01 12:41:32 -0700

    rendering of rectangles
    Former-commit-id: 3ccf234066bd21072e92e7727ef284ed05aeb304

--- go/infovis/export/Linear.go
+++ go/infovis/export/Linear.go
@@ -89,8 +89,88 @@ func cross(u [3]float32, v [3]float32) [3]float32 {
 }
 
 
+var qzero = [4]float32{0, 0, 0, 0}
+
+
 func quatFromPair(u [3]float32, s float32) [4] float32 {
   return [4]float32{u[0], u[1], u[2], s}
+}
+
+
+func qlength(u [4]float32) float32 {
+  return float32(math.Sqrt(float64(u[0] * u[0] + u[1] * u[1] + u[2]* u[2] + u[3] * u[3])))
+}
+
+
+func qnormalize(u [4]float32) [4]float32 {
+  ul := qlength(u)
+  if ul <= 0 {
+    return qzero
+  }
+  return [4]float32{u[0] / ul, u[1] / ul, u[2] / ul, u[3] / ul}
+}
+
+
+func conjugate(a [4]float32) [4]float32 {
+  return [4]float32{
+    -a[0],
+    -a[1],
+    -a[2],
+     a[3],
+  }
+}
+
+
+func multiply(a [4]float32, b [4]float32) [4]float32 {
+  ax := a[0]
+  ay := a[1]
+  az := a[2]
+  aw := a[3]
+  bx := b[0]
+  by := b[1]
+  bz := b[2]
+  bw := b[3]
+  return [4]float32{
+    ax * bw + aw * bx + ay * bz - az * by,
+    ay * bw + aw * by + az * bx - ax * bz,
+    az * bw + aw * bz + ax * by - ay * bx,
+    aw * bw - ax * bx - ay * by - az * bz,
+  }
+}
+
+
+func transformQuat(a [3]float32, q [4]float32) [3]float32 {
+  qx := q[0]
+  qy := q[1]
+  qz := q[2]
+  qw := q[3]
+  x := a[0]
+  y := a[1]
+  z := a[2]
+  uvx := qy * z - qz * y
+  uvy := qz * x - qx * z
+  uvz := qx * y - qy * x
+  uuvx := qy * uvz - qz * uvy
+  uuvy := qz * uvx - qx * uvz
+  uuvz := qx * uvy - qy * uvx
+  w2 := qw * 2
+  uvx *= w2
+  uvy *= w2
+  uvz *= w2
+  uuvx *= 2
+  uuvy *= 2
+  uuvz *= 2
+  return [3]float32{
+    x + uvx + uuvx,
+    y + uvy + uuvy,
+    z + uvz + uuvz,
+  }
+}
+
+
+func projectPlane(v [3]float32, u [3]float32) [3]float32 {
+  un := normalize(u)
+  return scaleAndAdd(v, un, - dot(v, un))
 }
 
 
@@ -106,3 +186,22 @@ func rotationFromVectorPair(v1 [3]float32, v2 [3]float32) [4]float32 {
   return q
 }
 
+
+func rotationFromPlane(xAxis [3]float32, yAxis [3]float32, origin [3]float32, xPoint [3]float32, yPoint [3]float32) [4]float32 {
+  return rotationFromVectorPairs(xAxis, yAxis, scaleAndAdd(xPoint, origin, -1), scaleAndAdd(yPoint, origin, -1))
+}
+
+
+func rotationFromVectorPairs(u0 [3]float32, v0 [3]float32, u2 [3]float32, v2 [3]float32) [4]float32 {
+  q2 := rotationFromVectorPair(u0, u2)
+  v1 := transformQuat(v2, conjugate(q2))
+  v0p := normalize(projectPlane(v0, u0))
+  v1p := normalize(projectPlane(v1, u0))
+  var q1 [4]float32
+  if equals(add(v0p, v1p), zero) {
+    q1 = quatFromPair(normalize(u0), 0)
+  } else {
+    q1 = rotationFromVectorPair(v0p, v1p)
+  }
+  return qnormalize(multiply(q2, q1))
+}
--- go/infovis/export/Render.go
+++ go/infovis/export/Render.go
@@ -21,10 +21,9 @@ func (builder *Builder) Render(geometry *model.Geometry) []uint32 {
   var nodes = []uint32{}
 
   var right = [3]float32{1, 0, 0}
-//var back  = [3]float32{0, 0, 1}
+  var back  = [3]float32{0, 0, 1}
   var noRotation = [4]float32{0, 0, 0, 1}
 
-  var mesh int8
   var color = decodeColor(geometry.Colr)
   var text = geometry.Text
 
@@ -46,6 +45,7 @@ func (builder *Builder) Render(geometry *model.Geometry) []uint32 {
   switch geometry.Type {
 
     case model.GEOMETRY_POINTS:
+      var mesh int8
       switch geometry.Glyp {
         case model.GLYPH_CUBE:
           mesh = model.MESH_CUBE
@@ -61,7 +61,6 @@ func (builder *Builder) Render(geometry *model.Geometry) []uint32 {
       }
 
     case model.GEOMETRY_POLYLINES:
-      mesh = model.MESH_LINE
       for _, positions := range positionss {
         for i := 1; i < len(positions); i++ {
           u0 := positions[i - 1]
@@ -70,9 +69,23 @@ func (builder *Builder) Render(geometry *model.Geometry) []uint32 {
           uc := scaleAndAdd(u0, ud, 0.5)
           r := rotationFromVectorPair(right, ud)
           s := [3]float32{length(ud), float32(geometry.Size), float32(geometry.Size)}
-          inode := builder.MakeNode(mesh, uc, r, s, color, text)
+          inode := builder.MakeNode(model.MESH_LINE, uc, r, s, color, text)
           nodes = append(nodes, inode)
         }
+      }
+
+    case model.GEOMETRY_RECTANGLES:
+      for _, positions := range positionss {
+        o := positions[0]
+        u := positions[1]
+        v := positions[2]
+        w := length(scaleAndAdd(u, o, -1))
+        h := length(scaleAndAdd(v, o, -1))
+        q := rotationFromPlane(right, back, o, u, v)
+        p := add(o, transformQuat([3]float32{w / 2, 0, h / 2}, q))
+        s := [3]float32{w, float32(geometry.Size), h}
+        inode := builder.MakeNode(model.MESH_LINE, p, q, s, color, text)
+        nodes = append(nodes, inode)
       }
 
   }
@@ -83,20 +96,6 @@ func (builder *Builder) Render(geometry *model.Geometry) []uint32 {
 
 
 /*
-    geometry.shape.polylines.map((polyline) => [
-      polyline.slice(0, polyline.length - 1)
-    , polyline.slice(1, polyline.length    )
-    ]).forEach(([u0s, u1s]) =>
-      u0s.map(function(u0, i) {
-        const u1 = u1s[i]
-        const ud = vec3.scaleAndAdd(vec3.create(), u1, u0, -1)
-        const uc = vec3.scaleAndAdd(vec3.create(), u0, ud, 0.5)
-        positions.push(uc)
-        rotations.push(Linear.rotationFromVectorPair(right, ud))
-        scales.push(vec3.fromValues(vec3.length(ud), geometry.size, geometry.size))
-      })
-    )
-
   } else if (Geometry.hasRectangles(geometry)) {
 
     geometry.shape.rectangles.forEach(function([o, u, v]) {
